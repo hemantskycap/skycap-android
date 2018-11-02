245f5d5ec41e2931184f4283f2c49d809a9e361f
Hemant Kaushik <hemant@skycap.co.in>
2018-11-02 21:03:56 +0530

    core - extension methods added core - changes in build.gradle

---
+++ core/src/main/java/skycap/android/core/ViewsExtensions.kt
@@ -0,0 +1,15 @@
+package skycap.android.core
+
+import android.view.View
+
+
+fun View.setVisibleOrGone(visibility: Boolean?) {
+    val visible = if (visibility == true) View.VISIBLE else View.GONE
+    this.visibility = visible
+}
+
+
+fun View.setVisibleOrInvisible(visibility: Boolean?) {
+    val visible = if (visibility == true) View.VISIBLE else View.INVISIBLE
+    this.visibility = visible
+}
--- example/src/main/java/skycap/android/MainActivity.kt
+++ example/src/main/java/skycap/android/MainActivity.kt
@@ -2,11 +2,17 @@ package skycap.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import skycap.android.core.setVisibleOrGone
+import skycap.android.core.setVisibleOrInvisible
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        message.setVisibleOrGone(true)
+        message.setVisibleOrInvisible(true)
     }
 }
