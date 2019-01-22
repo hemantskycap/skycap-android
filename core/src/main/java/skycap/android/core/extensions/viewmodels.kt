1bffa7a90e1007177d4075937f9e0ce099bde590
Hemant Sharma <hemant@skycap.co.in>
2019-01-22 12:25:11 +0530

    ViewModel extensions added.

---
+++ core/src/main/java/skycap/android/core/extensions/viewmodels.kt
@@ -0,0 +1,26 @@
+package skycap.android.core.extensions
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+
+class BaseViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>) = creator() as T
+}
+
+inline fun <reified T : ViewModel> Fragment.getViewModel(noinline creator: (() -> T)? = null): T {
+    return if (creator == null)
+        ViewModelProviders.of(this).get(T::class.java)
+    else
+        ViewModelProviders.of(this, BaseViewModelFactory(creator)).get(T::class.java)
+}
+
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(noinline creator: (() -> T)? = null): T {
+    return if (creator == null)
+        ViewModelProviders.of(this).get(T::class.java)
+    else
+        ViewModelProviders.of(this, BaseViewModelFactory(creator)).get(T::class.java)
+}
