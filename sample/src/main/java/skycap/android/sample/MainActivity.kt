93fcc2ae022e79bfa1356f805311cf16f1115849
Hemant Sharma <hemant@skycap.co.in>
2019-01-23 11:03:18 +0530

    Sample module added

---
+++ sample/src/main/java/skycap/android/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package skycap.android.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import skycap.android.core.setVisibleOrGone
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.showHideButton).setVisibleOrGone(true)
+    }
+}
