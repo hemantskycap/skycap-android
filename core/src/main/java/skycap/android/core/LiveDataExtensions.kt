56e825349db73e25d279103e0607f9ae1b92f3c0
Hemant Sharma <hemant@skycap.co.in>
2019-01-24 13:05:22 +0530

    Extension methods added for LiveData.

--- core/src/main/java/skycap/android/core/LiveDataExtensions.kt
+++ core/src/main/java/skycap/android/core/LiveDataExtensions.kt
@@ -1,22 +1,63 @@
 package skycap.android.core
 
+import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-fun <T> LiveData<T>.getDistinct(): LiveData<T> {
+fun <T : Any, L : LiveData<T>> observeForever(liveData: L, body: (T?) -> Unit): Observer<T> {
+    val observer = Observer<T>(body)
+    liveData.observeForever(observer)
+    return observer
+}
+
+fun <T : Any, L : LiveData<T>> observeForeverNonNull(liveData: L, body: (T) -> Unit): Observer<T> {
+    val observer = Observer<T> { it?.apply(body) }
+    liveData.observeForever(observer)
+    return observer
+}
+
+fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) -> Unit): Observer<T> {
+    val observer = Observer<T>(body)
+    liveData.observe(this, observer)
+    return observer
+}
+
+fun <T : Any, L : LiveData<T>> LifecycleOwner.observeNonNull(liveData: L, body: (T) -> Unit): Observer<T> {
+    val observer = Observer<T> { it?.apply(body) }
+    liveData.observe(this, observer)
+    return observer
+}
+
+/**
+ * Observe [LiveData] in blocking mode,
+ * Can be used in testing
+ * */
+fun <T> LiveData<T>.observeBlocking(count: Int = 1, waitInSeconds: Long = 2): T? {
+    var value: T? = null
+    val latch = CountDownLatch(count)
+
+    val observer = Observer<T> { t ->
+        value = t
+        latch.countDown()
+    }
+
+    observeForever(observer)
+
+    latch.await(waitInSeconds, TimeUnit.SECONDS)
+    return value
+}
+
+fun <T> LiveData<T>.distinct(): LiveData<T> {
     val distinctLiveData = MediatorLiveData<T>()
 
     distinctLiveData.addSource(this, object : Observer<T> {
-        private var initialized = false
         private var lastObj: T? = null
 
         override fun onChanged(obj: T?) {
-            if (!initialized) {
-                initialized = true
-                lastObj = obj
-                distinctLiveData.postValue(lastObj)
-            } else if ((obj == null && lastObj != null) || obj != lastObj) {
+            if (obj != lastObj) {
                 lastObj = obj
                 distinctLiveData.postValue(lastObj)
             }
